9cfdf100fefbf0b8d1775f1d4a9f78743998efb4
unknown <ankurjyotidas171@gmail.com>
2020-12-17 02:49:50 +0530

    changes made

--- src/index.js
+++ src/index.js
@@ -8,7 +8,25 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 const { connections } = require('mongoose');
 
+const collegeSchema = require('./schema');
+const { ReplSet } = require('mongodb');
 
+app.get('/findColleges', (req,res)=>{
+
+    // name, state, city, minPackage, maxFees, course and exams
+    let {name, state, city, minPackage, maxFees, course, exams} = req.body;
+    collegeSchema.find({name : {$regex : `/${name}.line/`, $options: 'si'}, 
+        state: {$regex : `/${state}/` , $options: 'i'},
+        city: {$regex: `/${city}/`, $options: 'i'},
+        minPackage : {$gte : Number(minPackage)},
+        maxFees : {$lte: Number(maxFees)},
+        course: {$regex: `/${course}/`, $options: 'i'},
+        exams: {$regex: `/${exams}.line/`, $options: 'si'} 
+        })
+        .then(result=> res.send(result))
+        .catch(error=> res.status(400).send('college not found'));
+
+});
 
 app.listen(port, () => console.log(`App listening on port ${port}!`))
 
